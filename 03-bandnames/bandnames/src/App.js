18394d0c4d07eab9b6a8961163108719c3f31576
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2021-03-09 21:50:17 -0600

    useSocket - CustomHook

--- 03-bandnames/bandnames/src/App.js
+++ 03-bandnames/bandnames/src/App.js
@@ -1,40 +1,12 @@
 import React, { useEffect, useState } from 'react';
-// import io from 'socket.io-client';
+import { useSocket } from './hooks/useSocket';
 import { BandAdd } from './components/BandAdd';
 import { BandList } from './components/BandList';
-import { useSocket } from './hooks/useSocket';
-
-// const connectSocketServer = () => {
-//   const socket = io.connect('http://localhost:8080', {
-//     transports: ['websocket']
-//   });
-//   return socket;
-// };
 
 function App() {
 
-  // const [ socket ] = useState(connectSocketServer());
-  // const [ online, setOnline ] = useState(false);
   const [bands, setBands] = useState([]);
-
   const { socket, online } = useSocket('http://localhost:8080');
-
-  // useEffect(() => { // Escucha si Socket conectado
-  //   console.log(socket);
-  //   setOnline(socket.connected);
-  // }, [socket]);
-
-  // useEffect(() => {
-  //   socket.on('connect', () => {
-  //     setOnline(true);
-  //   });
-  // }, [socket]);
-
-  // useEffect(() => {
-  //   socket.on('disconnect', () => {
-  //     setOnline(false);
-  //   });
-  // }, [socket]);
 
   useEffect(() => {
     socket.on('current-bands', (bands) => {
