8b54d888a37fa4d6961e00fb39eed93f124b2348
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2021-04-01 18:41:52 -0600

    Pantalla de Escritorio

--- 04-ticket-app/ticket-app/src/pages/Escritorio.js
+++ 04-ticket-app/ticket-app/src/pages/Escritorio.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import { Button, Col, Divider, Row, Typography } from 'antd';
+import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons';
+
+const { Title, Text } = Typography;
 
 export const Escritorio = () => {
 
+    const salir = () => {
+        console.log('Salir');
+    };
+
+    const siguienteTicket = () => {
+        console.log('Siguiente Ticket');
+    };
+
     return (
-        <div>
-            Escritorio Page
-        </div>
+        <>
+            <Row>
+                <Col span={ 20 }>
+                    <Title level={ 2 }>Emmanuel</Title>
+                    <Text>Usted está trabajando en el escritorio: </Text>
+                    <Text type="success">5</Text>
+                </Col>
+                <Col span={ 4 } align="right">
+                    <Button
+                        shape="round"
+                        type="danger"
+                        onClick={ salir }
+                    >
+                        <CloseCircleOutlined />
+                        Salir
+                    </Button>
+                </Col>
+            </Row>
+            <Divider />
+            <Row>
+                <Col>
+                    <Text>Está atendiendo el ticket número: </Text>
+                    <Text 
+                        style={{ fontSize: 30 }}
+                        type="danger"
+                    >
+                        55
+                    </Text>
+                </Col>
+            </Row>
+            <Row>
+                <Col offset={ 18 } span={ 6 } align="right">
+                    <Button
+                        onClick={ siguienteTicket }
+                        shape="round"
+                        type="primary"
+                    >
+                        <RightOutlined />
+                        Siguiente
+                    </Button>
+                </Col>
+            </Row>
+        </>
     );
 
 };
