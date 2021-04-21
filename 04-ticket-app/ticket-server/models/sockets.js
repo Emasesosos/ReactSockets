fe4699c1a8ed22593da3c4839f4fed0aa3767ca8
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2021-04-20 20:39:16 -0500

    Obtener un nuevo ticket

--- 04-ticket-app/ticket-server/models/sockets.js
+++ 04-ticket-app/ticket-server/models/sockets.js
@@ -1,27 +1,34 @@
-
+const TicketList = require('./ticket-list');
 
 class Sockets {
 
     constructor( io ) {
 
         this.io = io;
-
+        // Crear la instancia de nuestro ticketList
+        this.ticketList = new TicketList();
         this.socketEvents();
+
     }
 
     socketEvents() {
         // On connection
         this.io.on('connection', ( socket ) => {
 
-            console.log('Cliente conectado')
+            console.log('Cliente conectado');
 
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
+            socket.on('solicitar-ticket', (data, callback) => {
+                // console.log('Nuevo ticket backend');
+                const nuevoTicket = this.ticketList.crearTicket();
+                callback(nuevoTicket);
             });
-            
+
+            // // Escuchar evento: mensaje-to-server
+            // socket.on('mensaje-to-server', ( data ) => {
+            //     console.log( data );
+                
+            //     this.io.emit('mensaje-from-server', data );
+            // });     
         
         });
     }
