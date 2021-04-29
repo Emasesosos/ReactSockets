a77b7202c3b6dfe6d4a7d0a00c076b706e17def7
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2021-04-29 12:00:22 -0500

    Modelos necesarios en el backend

--- 05-mapas/mapas-server/models/sockets.js
+++ 05-mapas/mapas-server/models/sockets.js
@@ -1,29 +1,20 @@
-
-
+const Marcadores = require('./marcadores');
 class Sockets {
 
     constructor( io ) {
-
         this.io = io;
-
+        this.marcadores = new Marcadores();
         this.socketEvents();
     }
 
     socketEvents() {
         // On connection
         this.io.on('connection', ( socket ) => {
-
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
-        
+            // TODO: marcadores-activos  
+            // TODO: marcador-nuevo
+            // TODO: marcador-actualizado  
         });
     }
-
 
 }
 
