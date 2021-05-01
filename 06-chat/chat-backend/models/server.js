216809f9b07cf3302392d7d823ce8d1e2e2edb5c
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2021-05-01 15:29:07 -0500

    Definición de rutas de autenticación

--- 06-chat/chat-backend/models/server.js
+++ 06-chat/chat-backend/models/server.js
@@ -7,20 +7,17 @@ const cors     = require('cors');
 
 const Sockets  = require('./sockets');
 const { dbConnection } = require('../database/config');
+const router = require('../router/auth');
 
 class Server {
 
     constructor() {
-
         this.app  = express();
         this.port = process.env.PORT;
-
         // Conectar a DB
         dbConnection();
-
         // Http server
         this.server = http.createServer( this.app );
-        
         // Configuraciones de sockets
         this.io = socketio( this.server, { /* configuraciones */ } );
     }
@@ -28,10 +25,10 @@ class Server {
     middlewares() {
         // Desplegar el directorio público
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
-
         // CORS
         this.app.use( cors() );
-
+        // API ENDPOINTS
+        this.app.use('/api/login', router); // Auth
     }
 
     // Esta configuración se puede tener aquí o como propieda de clase
@@ -41,13 +38,10 @@ class Server {
     }
 
     execute() {
-
         // Inicializar Middlewares
         this.middlewares();
-
         // Inicializar sockets
         this.configurarSockets();
-
         // Inicializar Server
         this.server.listen( this.port, () => {
             console.log('Server corriendo en puerto:', this.port );
