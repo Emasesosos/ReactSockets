d40925f0f3e29a542500cf231177ee046207e178
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2021-05-03 00:19:17 -0500

    Crear usuario en base de datos

--- 06-chat/chat-backend/controllers/auth.js
+++ 06-chat/chat-backend/controllers/auth.js
@@ -1,14 +1,31 @@
 const { response } = require("express");
+const Usuario = require("../models/usuario");
 
 const crearUsuario = async(req, res = response) => {
-    const { nombre, email, password } = req.body;
-    res.json({
-        ok: true,
-        msg: 'register',
-        nombre,
-        email,
-        password
-    });
+    try {
+        const { email, password } = req.body;
+        // Verificar que el email no exista
+        const existeEmail = await Usuario.findOne({ email });
+        if(existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya existe'
+            });
+        }
+        // Encriptar Contraseña
+        // Guardar usuario en BD
+        const usuario = new Usuario(req.body);
+        await usuario.save();
+        res.json({
+            usuario
+        });
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
 };
 
 const login = async(req, res = response) => {
@@ -33,3 +50,33 @@ module.exports = {
     login,
     renewToken
 }
+
+// ENPOINTS PRUEBAS DE RESPONSE ...
+
+// const crearUsuario = async(req, res = response) => {
+//     const { nombre, email, password } = req.body;
+//     res.json({
+//         ok: true,
+//         msg: 'register',
+//         nombre,
+//         email,
+//         password
+//     });
+// };
+
+// const login = async(req, res = response) => {
+//     const { email, password } = req.body;
+//     res.json({
+//         ok: true,
+//         msg: 'login',
+//         email,
+//         password
+//     });
+// };
+
+// const renewToken = async(req, res = response) => {
+//     res.json({
+//         ok: true,
+//         msg: 'renew'
+//     });
+// };
