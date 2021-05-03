53c93f5c6b34a4076bd6e58ccf5ffe439050e381
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2021-05-02 23:48:22 -0500

    Middleware personalizado - ValidarCampos

--- 06-chat/chat-backend/controllers/auth.js
+++ 06-chat/chat-backend/controllers/auth.js
@@ -1,21 +1,17 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 
 const crearUsuario = async(req, res = response) => {
+    const { nombre, email, password } = req.body;
     res.json({
         ok: true,
-        msg: 'ABC'
+        msg: 'register',
+        nombre,
+        email,
+        password
     });
 };
 
 const login = async(req, res = response) => {
-    const errores = validationResult(req);
-    if(!errores.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errores.mapped()
-        });
-    }
     const { email, password } = req.body;
     res.json({
         ok: true,
--- 06-chat/chat-backend/router/auth.js
+++ 06-chat/chat-backend/router/auth.js
@@ -3,19 +3,25 @@
 */
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { crearUsuario } = require('../controllers/auth');
-const { login } = require('../controllers/auth');
-const { renewToken } = require('../controllers/auth');
+// Controladores
+const { crearUsuario, login, renewToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 // Crear nuevos usuarios
-router.post('/new', crearUsuario);
+router.post('/new',[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    validarCampos
+],crearUsuario);
 
 // Login
 router.post('/',[
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password es obligatorio').not().isEmpty()
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    validarCampos
 ],login);
 
 // Revalidar token
