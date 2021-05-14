9094fec087f45adc1d5cd0a5811018e29dca5603
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2021-05-14 12:10:26 -0500

    Pantalla de registro

--- 06-chat/chat-app/src/auth/AuthContext.js
+++ 06-chat/chat-app/src/auth/AuthContext.js
@@ -32,8 +32,22 @@ export const AuthProvider = ({ children }) => {
         return resp.ok;
     };
 
-    const register = (nombre, email, password) => {
-
+    const register = async(nombre, email, password) => {
+        const resp = await fetchSinToken('login/new', { nombre, email, password }, 'POST'); 
+        if(resp.ok) {
+            localStorage.setItem('token', resp.token);
+            const { usuario } = resp;
+            setAuth({
+                uid: usuario.uid,
+                checking: false,
+                logged: true,
+                name: usuario.nombre,
+                email: usuario.email,
+            });
+            console.log('¡Autenticado!');
+            return true;
+        }
+        return resp.msg;
     };
 
     const verificaToken = useCallback(() => {
