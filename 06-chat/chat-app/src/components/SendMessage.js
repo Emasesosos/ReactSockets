07e3c7e1f1f2dc8e7888725c3648bbb531bf3256
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2021-05-24 16:54:40 -0500

    ✨ Enviar un mensaje pt1

--- 06-chat/chat-app/src/components/SendMessage.js
+++ 06-chat/chat-app/src/components/SendMessage.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const SendMessage = () => {
+
+    const [mensaje, setMensaje] = useState('');
+
+    const onChange = ({ target }) => {
+        setMensaje(target.value);
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if(mensaje.length === 0) return;
+        setMensaje('');
+        // TODO: Emitir un evento de sockets para enviar el mensaje
+        // TODO: Hacer el dispatch del mensaje... 
+    };
+
     return (
-        <form>
+        <form
+            onSubmit={ onSubmit }
+        >
             <div className="type_msg row">
                 <div className="input_msg_write col-sm-9">
-                    <input type="text" className="write_msg" placeholder="Mensaje..." />
+                    <input 
+                        type="text" 
+                        className="write_msg" 
+                        placeholder="Mensaje..." 
+                        value={ mensaje }
+                        onChange={ onChange }
+                    />
                 </div>
                 <div className="col-sm-3 text-center">
                     <button className="msg_send_btn mt-3" type="submit">
